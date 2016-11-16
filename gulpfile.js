a0d720409c2c7f256534cb77b5c14ac3d7b4b2c4
Hadyk, Alan <alan.hadyk@sap.com>
2016-11-16 21:13:46 +0100

    Added wireframe, updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
 
