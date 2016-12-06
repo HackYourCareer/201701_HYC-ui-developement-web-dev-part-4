2c6d161a8e11e61a2ba786b58073129999a2987f
Hadyk, Alan <alan.hadyk@sap.com>
2016-12-06 22:12:50 +0100

    Changed Sass output style to expanded

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
 
